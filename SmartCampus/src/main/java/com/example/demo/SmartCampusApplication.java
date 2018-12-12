17f05a0e8a02fd7e49d51e4f42c210f70df623f0
dell <dell@DESKTOP-OFR6RCO>
2018-12-12 15:55:02 +0800

    123456

--- SmartCampus/src/main/java/com/example/demo/SmartCampusApplication.java
+++ SmartCampus/src/main/java/com/example/demo/SmartCampusApplication.java
@@ -8,5 +8,6 @@ public class SmartCampusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartCampusApplication.class, args);
+		System.out.println("123456");
 	}
 }
